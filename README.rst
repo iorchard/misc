a7337c6cc77bfc092c0b3124babaa1fbbe587296
Heechul Kim <jijisa@iorchard.net>
2019-01-10 16:07:43 +0900

    * Added rst2what.sh * Modified README.rst to explain rst2what.sh script.

--- README.rst
+++ README.rst
@@ -1,4 +1,39 @@
 misc
 ====
 
-This is a repo for trivial things when we work.
+This is a repo for trivial things while we work.
+
+rst2what.sh
+------------
+
+This is a script to convert a rst(restructured text) file to the other
+formatted file like docx, pdf, etc.
+
+Prerequisites
++++++++++++++++
+
+Install these packages to use this script.::
+
+    $ sudo apt install -y pandoc texlive texlive-xetex fonts-nanum
+
+How to use
++++++++++++
+
+Usage)::
+
+    Usage: ./rst2what.sh -f <infile> -t [pdf|docx|...] [-o <outfile>] 
+
+Here are some examples.
+
+Convert a.rst to a.pdf.::
+
+     $ ./rst2what.sh -f a.rst -t pdf
+
+Convert b.rst to my.pdf.::
+
+     $ ./rst2what.sh -f b.rst -t pdf -o my.pdf
+
+Convert c.rst to c.docx.::
+
+     $ ./rst2what.sh -f c.rst -t docx 
+
---
+++ rst2what.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# rst2what.sh: Convert rst file to whatever file using pandoc.
+#
+
+USAGE() {
+    echo "Usage: $0 -f <infile> -t [pdf|docx|...] [-o <outfile>] " 1>&2
+    exit 1
+}
+INFILE=""
+OTYPE=""
+OUTFILE=""
+
+# Check arguments.
+while getopts f:t:o: opt
+do
+    case "${opt}" in
+        f)
+            INFILE="${OPTARG}"
+            ;;
+        t)
+            OTYPE="${OPTARG}"
+            ;;
+        o)
+            OUTFILE="${OPTARG}"
+            ;;
+        \?)
+            USAGE
+            exit 1
+            ;;
+    esac
+done
+
+# Check if ${INFILE} exists.
+if [ ! -f "${INFILE}" ]
+then
+    echo "Error) No such file: ${INFILE}"
+    USAGE
+    exit 1
+fi
+# Check if the directory of ${OUTFILE} exists.
+if [ "${OUTFILE}" == "" ]
+then
+    DIR="."
+    OUTFILE=$(basename ${INFILE} | cut -d'.' -f1)
+else
+    DIR=$(dirname ${OUTFILE})
+fi
+if [ ! -d "${DIR}" ]
+then
+    echo "Warn) No such directory: ${DIR}."
+fi
+
+case $OTYPE in
+    pdf)
+        echo "Info) Convert to pdf."
+        echo "Debug) pandoc -s --latex-engine=xelatex -o ${DIR}/${OUTFILE}.pdf -V mainfont='NanumMyeongjo' ${INFILE}"
+        pandoc -s --latex-engine=xelatex \
+            -o ${DIR}/${OUTFILE}.pdf \
+            -V mainfont='NanumMyeongjo' ${INFILE}
+        ;;
+    *)
+        echo "Info) Convert to ${OTYPE}."
+        echo "Debug) pandoc -s -t ${OTYPE} -o ${DIR}/${OUTFILE}.${OTYPE} ${INFILE}"
+        pandoc -s -t ${OTYPE} -o ${DIR}/${OUTFILE}.${OTYPE} ${INFILE}
+esac
+
